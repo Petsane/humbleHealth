7e671444ffe322df58ed97a4aea936a4d895e5af
Tumelo <tumelopetsane.tp@gmail.com>
2023-06-05 10:58:25 +0200

    first commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        return view('Dashboard');
+    }
+    public function about(){
+        return view('about');
+    }
+    public function buy(){
+        return view('buy');
+    }
+    public function online(){
+        return view('online');
+    }
+    public function catalogue(){
+        return view('catalogue');
+    }
+    public function contact(){
+        return view('contact');
+    }
+}
