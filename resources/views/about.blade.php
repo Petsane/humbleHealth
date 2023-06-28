33bcc8503d1c9357c145f7e124d4a5e8ccdfb6c6
Tumelo <tumelopetsane.tp@gmail.com>
2023-06-28 14:48:53 +0200

    about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,11 @@
 <x-app-layout>
-    
+    <div>
+        <img style="width:100%; height: 300px" src="https://www.eomega.org/sites/default/files/styles/scale_width_1440/public/hero/omega_institute_homeopathy_0.jpg?itok=iyOjARxh" alt="">
+    </div>
+    <div style="background-image: url(/images/hjk4.jpg)">
+        <div>
+            <p>Humble remedies company is a dedicated supplier of all homeopathic medicines including polycrests, small remedies, organopathics</p>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Explore our products</button>
+        </div>
+    </div>
 </x-app-layout>
